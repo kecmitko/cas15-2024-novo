a006864388266c221fc651a822ddae85b06bf371
samedsain <samedsain65@gmail.com>
2024-05-15 19:09:01 +0100

    added name and surname

--- Git-test/Program.cs
+++ Git-test/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Mitko Keckaroski");
 Console.WriteLine("Local time: "+DateTime.Now);
 Console.WriteLine("Utc time: "+DateTime.UtcNow);
-
+Console.WriteLine("Samed Sain");
