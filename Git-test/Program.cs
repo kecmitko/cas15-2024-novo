11cf71a15c1161477ba96f61c6efe8428b574bb7
Mitko Keckaroski <kecmitko@gmail.com>
2024-05-15 18:57:24 +0200

    update

--- Git-test/Program.cs
+++ Git-test/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mitko Keckaroski");
-Console.WriteLine("local time: "+DateTime.Now);
+Console.WriteLine("Local time: "+DateTime.Now);
 Console.WriteLine("Utc time: "+DateTime.UtcNow);
 
